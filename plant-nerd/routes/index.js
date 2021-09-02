cf10b1d9e18a9eb55d7c2f1c79b9f61279e9b3de
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-09-02 19:18:23 +0200

    fix redirect after login

--- plant-nerd/routes/index.js
+++ plant-nerd/routes/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("main");
+router.get('/', (req, res, next) => {
+  res.render('main');
+});
+
+router.get('/explore', (req, res, next) => {
+  res.render('insidePlants/plantsMain');
 });
 
 module.exports = router;
