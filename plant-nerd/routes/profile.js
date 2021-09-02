8c22ce0f70f9427790d2c569dd2b6bda50257aca
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-09-02 19:55:30 +0200

    delete double route

--- plant-nerd/routes/profile.js
+++ plant-nerd/routes/profile.js
@@ -22,17 +22,4 @@ router.post('/deleteProfile/:id/delete', (req, res) => {
   });
 });
 
-router.get('/user/my-posts', redirectLoggedIn, (req, res) => {
-  const userID = req.session.currentUser._id;
-  Plant.find({author: userID})
-    .then((posts) => {
-      res.render('insidePlants/userPosts', {
-        posts: posts,
-      });
-    })
-    .catch((error) =>
-      console.log(`Error while searching for user posts: ${error}`)
-    );
-});
-
 module.exports = router;
