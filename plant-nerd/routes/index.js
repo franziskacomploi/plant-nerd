1ba2c2590b141a5a9a2c2aa70d32f8581f43ee5e
franziska pendzialek <franziska.pendzialek@outlook.com>
2021-09-04 11:24:37 +0200

    add all plants view to explore page

--- plant-nerd/routes/index.js
+++ plant-nerd/routes/index.js
@@ -2,14 +2,29 @@ const express = require('express');
 const router = express.Router();
 const {redirectLoggedIn} = require('./guards/guards');
 
+const Plant = require('../models/plant.model');
+
 /* GET home page */
 router.get('/', (req, res, next) => {
   res.render('main');
 });
 
 router.get('/explore', redirectLoggedIn, (req, res, next) => {
-  res.render('insidePlants/plantsMain', {
-    userInSession: req.session.currentUser,
+  Plant.find().then((result) => {
+    const compare = (a, b) => {
+      if (a.foundOnDate < b.foundOnDate) {
+        return 1;
+      }
+      if (a.foundOnDate > b.foundOnDate) {
+        return -1;
+      }
+      return 0;
+    };
+    const plants = result.sort(compare);
+    res.render('insidePlants/plantsMain', {
+      userInSession: req.session.currentUser,
+      plants: plants,
+    });
   });
 });
 
